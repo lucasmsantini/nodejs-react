91f781fbb5689f926cfa1b59e96be403d1fa9c53
Lucas <lucas.santini@nddigital.com.br>
2020-10-21 15:08:44 -0300

    teste de comit

---
+++ bin/base/repository-base.js
@@ -0,0 +1,15 @@
+exports.post = async (repository,validationContract,req,res)=>{
+    try {
+        let data = req.body;
+
+        if(!validationContract.isValid()){
+            res.status(400).send({message:'Existem dados inválidos em sua requisição', validation:validationContract.errors()}).end();
+            return;
+        }
+        let resultado = await repository.create(data,req);
+        res.status(201).send(resultado);
+    } catch (e) {
+        console.log(e);
+        res.status(500).send({message:"Internal server error", error: e});
+    }
+}
