52205c900e8357d70446387332f6c47dc5ef77d8
Lucas <lucas.santini@nddigital.com.br>
2020-10-21 16:42:24 -0300

    user-controller

--- bin/base/repository-base.js
+++ bin/base/repository-base.js
@@ -3,13 +3,59 @@ exports.post = async (repository,validationContract,req,res)=>{
         let data = req.body;
 
         if(!validationContract.isValid()){
-            res.status(400).send({message:'Existem dados inválidos em sua requisição', validation:validationContract.errors()}).end();
+            res.status(400).send({message:'Existem dados inválidos em sua requisição - post', validation:validationContract.errors()}).end();
             return;
         }
         let resultado = await repository.create(data,req);
         res.status(201).send(resultado);
     } catch (e) {
         console.log(e);
-        res.status(500).send({message:"Internal server error", error: e});
+        res.status(500).send({message:"Internal server error - post", error: e});
+    }
+};
+
+exports.put = async (repository,validationContract,req,res)=>{
+    try {
+        let data = req.body;
+
+        if(!validationContract.isValid()){
+            res.status(400).send({message:'Existem dados inválidos em sua requisição - put', validation:validationContract.errors()}).end();
+            return;
+        }
+        let resultado = await repository.update(req.params.id,data,req.usuarioLogado.user);
+        res.status(202).send(resultado);
+    } catch (e) {
+        console.log(e);
+        res.status(500).send({message:"Internal server error - put", error: e});
+    }
+};
+
+exports.get = async (repository,validationContract,req,res)=>{
+    try {
+       
+        let resultado = await repository.getAll();
+        res.status(200).send(resultado);
+    } catch (e) {
+        console.log(e);
+        res.status(500).send({message:"Internal server error - get", error: e});
+    }
+};
+
+exports.delete = async (repository,validationContract,req,res)=>{
+    try {
+       const id = req.params.id;
+       if(id){
+           let resultado = await repository.delete(id,req.usuarioLogado);
+           if(resultado !== "Operação inválida - delete"){
+                res.status(200).send({message:"Registro excluído com sucesso"});
+           } else {
+                res.status(401).send({message:"Operação inválida -- delete"});
+           }
+       } else {
+           res.status(500).send({message:"O parâmetro ID precisa ser informado - delete"});
+       }
+    } catch (e) {
+        console.log(e);=
+        res.status(500).send({message:"Internal server error - delete", error: e});
     }
 }
---
+++ controllers/user-controller.js
@@ -0,0 +1,90 @@
+'user strict';
+
+const repository = require('../repositories/user-repository.js');
+const validation = require('../bin/helpers/validation.js');
+const ctrlBase = require('../bin/base/controller-base.js');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const jvariables = require('../bin/configuration/variables.js');
+const _repo = new repository();
+
+function userController(){}
+
+userController.prototype.post = async (req,res) => {
+    let _validationContract = new validation();
+    _validationContract.isRequired(req.body.nome, "Informe seu nome: ");
+    _validationContract.isRequired(req.body.email, "Informe seu email: ");
+    _validationContract.isRequired(req.body.senha, "Informe sua senha: ");
+    _validationContract.isRequired(req.body.senhaConfirmacao, "Confirme sua senha: ");
+    _validationContract.isTrue(req.body.senhaConfirmacao !== req.body.senha, "As senhas não são iguais. ");
+    _validationContract.isEmail(req.body.email, "Email inválido. ");
+    
+    try {
+        let usuarioEmailExiste = await_repo.isEmailExste(req.body.email);
+        if (usuarioEmailExiste){
+            _validationContract.isTrue(usuarioEmailExiste.nome != undefined, `Já existe o email ${req.body.email} cadastrado no banco de dados.`);
+        }
+        var salt = await bcrypt.genSaltSync(10);
+        req.body.senha = await bcrypt.hashSync(req.body.senha, salt);
+        ctrlBase.post(_repo,_validationContract,req,res);
+    } catch (e) {
+        res.status(500).send({message: "Internal server error - usercontroller post", error: e});
+    }
+};
+
+userController.prototype.put = async (req,res) => {
+    let _validationContract = new validation();
+    _validationContract.isRequired(req.body.nome, "Informe seu nome: ");
+    _validationContract.isRequired(req.params.id, "Informe seu id: ");
+    _validationContract.isRequired(req.body.email, "Informe seu email: ");
+    _validationContract.isRequired(req.body.senha, "Informe sua senha: ");
+    _validationContract.isRequired(req.body.senhaConfirmacao, "Confirme sua senha: ");
+    _validationContract.isTrue(req.body.senhaConfirmacao !== req.body.senha, "As senhas não são iguais. ");
+    _validationContract.isEmail(req.body.email, "Email inválido. ");
+    
+    try {
+        let usuarioEmailExiste = await_repo.isEmailExste(req.body.email);
+        if (usuarioEmailExiste){
+            _validationContract.isTrue(usuarioEmailExiste.nome != undefined && usuarioEmailExiste._id != req.params.id, `Já existe o email ${req.body.email} cadastrado no banco de dados.`);
+        }
+        ctrlBase.put(_repo,_validationContract,req,res);
+    } catch (e) {
+        res.status(500).send({message: "Internal server error - usercontroller post", error: e});
+    }
+};
+
+userController.prototype.get = async(req,ses) =>{
+    ctrlBase.get(_repo,req,res);
+};
+
+userController.prototype.delete = async(req,ses) =>{
+    _validationContract.isRequired(req.params.id, "Informe seu id: ");
+    ctrlBase.delete(_repo,req,res);
+};
+
+userController.prototype.authenticate = async (req,res) => {
+    let _validationContract = new validation();
+
+    _validationContract.isRequired(req.body.email, "Informe seu email: ");
+    _validationContract.isRequired(req.body.senha, "Informe sua senha: ");
+    _validationContract.isRequired(req.body.senhaConfirmacao, "Confirme sua senha: ");
+    _validationContract.isTrue(req.body.senhaConfirmacao !== req.body.senha, "As senhas não são iguais. ");
+    _validationContract.isEmail(req.body.email, "Email inválido. ");
+    if (!_validationContract.isValid()){
+        res.status(400).send({message:"Não foi possível efetuar o login",validation: _validationContract.errors()
+    });
+    return
+    }
+    let usuarioEncontrado = await _repo.authenticate(req.body.email, req.body.senha, false);
+    if (usuarioEncontrado == null){
+        res.status(404).send({message:"Usuário ou senha inválidos"});
+    }
+    if(usuarioEncontrado){
+        res.status(200).senha({usuario:usuarioEncontrado, token:jwt.sign({user:usuarioEncontrado},variables.Security.secretKey)});
+    } else {
+        res.status(404).send({message:"Usuário ou senha inválidos"});
+
+    }
+}
+
+module.exports = userController;
