d3e254018985071c67d876346c04553a53409289
Lucas <lucas.santini@nddigital.com.br>
2020-10-22 08:33:53 -0300

    repository-base e controller-base

--- bin/base/repository-base.js
+++ bin/base/repository-base.js
@@ -1,61 +1,26 @@
-exports.post = async (repository,validationContract,req,res)=>{
-    try {
-        let data = req.body;
+'use strict'
 
-        if(!validationContract.isValid()){
-            res.status(400).send({message:'Existem dados inválidos em sua requisição - post', validation:validationContract.errors()}).end();
-            return;
-        }
-        let resultado = await repository.create(data,req);
-        res.status(201).send(resultado);
-    } catch (e) {
-        console.log(e);
-        res.status(500).send({message:"Internal server error - post", error: e});
+const mongoose = require('mongoose');
+
+class baseRepository {
+    constructor(model){
+        this._model = mongoose.model(model);
     }
-};
-
-exports.put = async (repository,validationContract,req,res)=>{
-    try {
-        let data = req.body;
-
-        if(!validationContract.isValid()){
-            res.status(400).send({message:'Existem dados inválidos em sua requisição - put', validation:validationContract.errors()}).end();
-            return;
-        }
-        let resultado = await repository.update(req.params.id,data,req.usuarioLogado.user);
-        res.status(202).send(resultado);
-    } catch (e) {
-        console.log(e);
-        res.status(500).send({message:"Internal server error - put", error: e});
+    async create(data){
+        let modelo = new this._model(data);
+        let resultado = await modelo.save();
+        return resultado;
+    }unc update (id,data,usuarioLogado){
+        await this._model.findByIdAndUpdate(id,{$set:data});
+        let resultado = await this._model.findById(id);
+        return resultado;
     }
-};
-
-exports.get = async (repository,validationContract,req,res)=>{
-    try {
-       
-        let resultado = await repository.getAll();
-        res.status(200).send(resultado);
-    } catch (e) {
-        console.log(e);
-        res.status(500).send({message:"Internal server error - get", error: e});
+    async getAll(){
+        return await this._model.find();
     }
-};
-
-exports.delete = async (repository,validationContract,req,res)=>{
-    try {
-       const id = req.params.id;
-       if(id){
-           let resultado = await repository.delete(id,req.usuarioLogado);
-           if(resultado !== "Operação inválida - delete"){
-                res.status(200).send({message:"Registro excluído com sucesso"});
-           } else {
-                res.status(401).send({message:"Operação inválida -- delete"});
-           }
-       } else {
-           res.status(500).send({message:"O parâmetro ID precisa ser informado - delete"});
-       }
-    } catch (e) {
-        console.log(e);=
-        res.status(500).send({message:"Internal server error - delete", error: e});
+    async delete(id){
+        return await this._model.findByIdAndDelete(id);
     }
 }
+
+module.exports = baseRepository;
